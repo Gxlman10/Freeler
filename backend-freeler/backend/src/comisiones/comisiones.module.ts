8a763e80d95eb99db3e74ae8300d2e5a0448438f
Gelman Rony Chávez Medina <n00281961@upn.pe>
2025-10-13 16:26:12 -0500

    Agregando muchos endpoints

--- backend-freeler/backend/src/comisiones/comisiones.module.ts
+++ backend-freeler/backend/src/comisiones/comisiones.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ComisionesController } from './presentation/comisiones.controller';
+import { COMISION_REPOSITORY } from './application/interfaces/comision.repository.interface';
+import { ComisionEntity } from './infrastructure/entities/comision.entity';
+import { EstadoComisionEntity } from './infrastructure/entities/estado-comision.entity';
+import { TypeormComisionRepository } from './infrastructure/repositories/typeorm-comision.repository';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([ComisionEntity, EstadoComisionEntity])],
   controllers: [ComisionesController],
-  providers: [],
-  exports: [],
+  providers: [
+    { provide: COMISION_REPOSITORY, useClass: TypeormComisionRepository },
+    PayCommissionUseCase,
+  ],
+  exports: [COMISION_REPOSITORY],
 })
 export class ComisionesModule {}
